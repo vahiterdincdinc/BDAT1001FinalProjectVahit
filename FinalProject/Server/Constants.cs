fae76a0fe4af80eb5930a471544620e22e450ec8
Vahit <vahit.erdinc@gmail.com>
2021-04-21 00:27:54 -0400

    descriptive comment for code changes

---
+++ FinalProject/Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Issuer = Audiance;
+        public const string Audiance = "https://localhost:44382/";
+        public const string Secret = "not_too_short_secret_otherwise_it_might_error";
+    }
+}
